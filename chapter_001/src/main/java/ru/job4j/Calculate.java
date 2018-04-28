f15d349b4ea321ca120a3fb689c1ea52b3939e8c
IgorAntov <igor3377@yandex.ru>
2018-04-28 16:20:32 +0500

    1.4 JUnit[#51299]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,19 +1,27 @@
-/**
-* Class Calculate для вычисления арифметических операций 
-* @author Antropov
-* @since 28.04.2018 
-* @version 1
-* Main.
-* @param args -args.
-*/
-
 package ru.job4j;
 
+/**
+ * Calculate.
+ *
+ * @author Petr Arsentev (parsentev@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 public class Calculate {
-	
-	public static void main(String[] args){
-	
-	System.out.println("Hellow World");	
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 
-	}
-} 
+    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
+}
