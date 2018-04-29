a2ecb1034985e326a2e33b1188311a9712cd1f1c
IgorAntov <igor3377@yandex.ru>
2018-04-29 09:31:31 +0500

    3.3 Идеальный вес[#51384]

---
+++ chapter_001/src/test/java/ru/job4j/FitTest.java
@@ -0,0 +1,31 @@
+package ru.job4j;
+
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Igor Antropov
+ * @version $Id$
+ * @since 0.1
+ */
+public class FitTest {
+
+    @Test
+    public void manWeight() {
+        Fit fit = new Fit();
+        double weight = fit.manWeight(180);
+        assertThat(weight, closeTo(92.0, 0.1));
+    }
+
+    @Test
+    public void womanWeight() {
+        Fit fit = new Fit();
+        double weight = fit.manWeight(170);
+        assertThat(weight, closeTo(80.5, 0.1));
+    }
+}
