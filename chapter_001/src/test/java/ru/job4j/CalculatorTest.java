836008abbb4868cec27d65128bdc568771ed17f8
IgorAntov <igor3377@yandex.ru>
2018-04-29 06:47:57 +0500

    3.1 Элементарный калькулятор.[#51375]

---
+++ chapter_001/src/test/java/ru/job4j/CalculatorTest.java
@@ -0,0 +1,57 @@
+package ru.job4j;
+import com.sun.org.apache.bcel.internal.generic.D2F;
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ * @author Igor Antropov
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class CalculatorTest {
+
+    /**
+     * Test echo.
+     */
+
+    @Test
+    public void  whenAddOnePlusOneThenTwo() {
+        Calculator calc = new Calculator();
+        calc.add(1D, 1D);
+        double result = calc.getResult();
+        double expect = 2D;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void  whenSubtractOnesubOneThenTwo() {
+        Calculator calc = new Calculator();
+        calc.subtract(10D, 5D);
+        double result = calc.getResult();
+        double expect = 5D;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void  whenDivOneDivOneThenTwo() {
+        Calculator calc = new Calculator();
+        calc.div(10D, 5D);
+        double result = calc.getResult();
+        double expect = 2D;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void  whenMultipleOneMultipleOneThenTwo() {
+        Calculator calc = new Calculator();
+        calc.multiple(5D, 2D);
+        double result = calc.getResult();
+        double expect = 10D;
+        assertThat(result, is(expect));
+    }
+}
