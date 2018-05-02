6445d80552aaedabbb140ee9b6c8a5f76bd93179
IgorAntov <igor3377@yandex.ru>
2018-05-03 02:13:05 +0500

    Крестики-нолики на JavaFX[#51804]

--- chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -8,53 +8,32 @@ public class Logic3T {
     }
 
     public boolean isWinnerX() {
-        String str  = "";
-        int tmp = 0;
-        boolean result;
-        int count = 0;
-        for (int i = 0; i < table.length; i++) {
-            for (int j = 0; j < table[0].length; j++) {
-                if (table[i][j].hasMarkX()) {
-                    str = str + "1";
-                } else {
-                    str = str + "0";
-                }
-                count++;
-            }
-        }
-        tmp = Integer.parseInt(str, 2);
-        if (((tmp & 448) == 448) || ((tmp & 56) == 56) || ((tmp & 7) == 7)
-                || ((tmp & 292) == 292) || ((tmp & 146) == 146) || ((tmp & 73) == 73)
-                || ((tmp & 273) == 273) || ((tmp & 84) == 84)) {
+
+        boolean result = false;
+        if  (table[0][0].hasMarkX() & table[0][1].hasMarkX() & table[0][2].hasMarkX()
+                | table[1][0].hasMarkX() & table[1][1].hasMarkX() & table[1][2].hasMarkX()
+                | table[2][0].hasMarkX() & table[2][1].hasMarkX() & table[2][2].hasMarkX()
+                | table[0][0].hasMarkX() & table[1][0].hasMarkX() & table[2][0].hasMarkX()
+                | table[0][1].hasMarkX() & table[1][1].hasMarkX() & table[2][1].hasMarkX()
+                | table[0][2].hasMarkX() & table[1][2].hasMarkX() & table[2][2].hasMarkX()
+                | table[0][0].hasMarkX() & table[1][1].hasMarkX() & table[2][2].hasMarkX()
+                | table[2][0].hasMarkX() & table[1][1].hasMarkX() & table[1][2].hasMarkX()) {
             result = true;
-        } else {
-            result = false;
         }
         return result;
     }
 
     public boolean isWinnerO() {
-        String str = "";
-        int tmp = 0;
-        boolean result;
-        int count = 0;
-        for (int i = 0; i < table.length; i++) {
-            for (int j = 0; j < table[0].length; j++) {
-                if (table[i][j].hasMarkO()) {
-                    str = str + "1";
-                } else {
-                    str = str + "0";
-                }
-                count++;
-            }
-        }
-        tmp = Integer.parseInt(str, 2);
-        if (((tmp & 448) == 448) || ((tmp & 56) == 56) || ((tmp & 7) == 7)
-                || ((tmp & 292) == 292) || ((tmp & 146) == 146) || ((tmp & 73) == 73)
-                || ((tmp & 273) == 273) || ((tmp & 84) == 84)) {
+        boolean result = false;
+        if  (table[0][0].hasMarkO() & table[0][1].hasMarkO() & table[0][2].hasMarkO()
+                | table[1][0].hasMarkO() & table[1][1].hasMarkO() & table[1][2].hasMarkO()
+                | table[2][0].hasMarkO() & table[2][1].hasMarkO() & table[2][2].hasMarkO()
+                | table[0][0].hasMarkO() & table[1][0].hasMarkO() & table[2][0].hasMarkO()
+                | table[0][1].hasMarkO() & table[1][1].hasMarkO() & table[2][1].hasMarkO()
+                | table[0][2].hasMarkO() & table[1][2].hasMarkO() & table[2][2].hasMarkO()
+                | table[0][0].hasMarkO() & table[1][1].hasMarkO() & table[2][2].hasMarkO()
+                | table[2][0].hasMarkO() & table[1][1].hasMarkO() & table[1][2].hasMarkO()) {
             result = true;
-        } else {
-            result = false;
         }
         return result;
     }
