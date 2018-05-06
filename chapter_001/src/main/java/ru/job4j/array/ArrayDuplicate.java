2cc6fe265637ef3591eedf44b0681db312002913
IgorAntov <igor3377@yandex.ru>
2018-05-06 07:03:11 +0500

    new addition

--- chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -15,8 +15,8 @@ public class ArrayDuplicate {
      */
     public String[] remove(String[] array) {
         int unique = array.length;
-        for (int i = 0; i < unique ; i++) {
-            for (int j = i + 1; j < unique ; j++) {
+        for (int i = 0; i < unique; i++) {
+            for (int j = i + 1; j < unique; j++) {
                 if (array[i].equals(array[j])) {
                     array[j] = array[unique - 1];
                     unique--;
