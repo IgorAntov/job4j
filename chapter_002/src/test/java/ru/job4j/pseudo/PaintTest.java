8d2469bda5a206040d2bdda8991050531e32b1bd
IgorAntov <igor3377@yandex.ru>
2018-05-08 19:44:36 +0500

    4. Используя шаблон проектирования - стратегию[#52554]

---
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -0,0 +1,71 @@
+package ru.job4j.pseudo;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Igor Antropov
+ * @version $Id$
+ * @since 0.1
+ */
+public class PaintTest {
+
+    /**
+     * Test вывод формы - квадрат
+     */
+    @Test
+    public void whenDrawSquare() {
+        // получаем ссылку на стандартный вывод в консоль.
+        PrintStream stdout = System.out;
+        // Создаем буфур для хранения вывода.
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        //Заменяем стандартный вывод на вывод в пямять для тестирования.
+        System.setOut(new PrintStream(out));
+        // выполняем действия пишушиее в консоль.
+        new Paint().draw(new Square());
+        // проверяем результат вычисления
+        assertThat(
+                new String(out.toByteArray()),
+                is(
+                        new StringBuilder()
+                                .append("+++++++\n")
+                                .append("+     +\n")
+                                .append("+     +\n")
+                                .append("+++++++\n")
+                                .append(System.lineSeparator())
+                                .toString()
+                )
+        );
+        // возвращаем обратно стандартный вывод в консоль.
+        System.setOut(stdout);
+    }
+
+    /**
+     * Test вывод формы - треугольник
+     */
+    @Test
+    public void whenDrawTriangle() {
+        PrintStream stdout = System.out;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        new Paint().draw(new Triangle());
+        assertThat(
+                new String(out.toByteArray()),
+                is(
+                        new StringBuilder()
+                        .append("    +\n")
+                        .append("  +   +\n")
+                        .append(" +      +\n")
+                        .append("++++++++++\n")
+                        .append(System.lineSeparator())
+                        .toString()
+                )
+        );
+        System.setOut(stdout);
+    }
+}
