aa82ee8c65a820068f7f0d6c31671ba933255328
IgorAntov <igor3377@yandex.ru>
2018-06-08 10:20:08 +0500

    8. Каркас шахматной доски[#55709]

--- chapter_002/src/main/java/ru/job4j/chess/figures/black/BishopBlack.java
+++ chapter_002/src/main/java/ru/job4j/chess/figures/black/BishopBlack.java
@@ -27,40 +27,29 @@ public class BishopBlack implements Figure {
 
     @Override
     public Cell[] way(Cell source, Cell dest) {
+
         Cell[] steps = new Cell[0];
         int step = 0;
-        int sign1 = 1, sign2 = -1;
 
         if (abs(dest.x - source.x) != abs(dest.y - source.y)) {
             throw new ImpossibleMoveException();
         }
 
         steps = new Cell[abs(dest.x - source.x)];
-
-        if (dest.x > source.x && dest.y < source.y) {
-            getStep(source, dest, steps, step, sign1, sign2);
+        for (Cell cell : Cell.values()) {
+            if ((dest.x < cell.x && cell.x < source.x && dest.y < cell.y && cell.y < source.y) ||
+                    (dest.x > cell.x && cell.x > source.x && dest.y < cell.y && cell.y < source.y) ||
+                    (dest.x < cell.x && cell.x < source.x && dest.y > cell.y && cell.y > source.y) ||
+                    (dest.x > cell.x && cell.x > source.x && dest.y > cell.y && cell.y > source.y))
+            {
+                steps[step] = cell;
+                step++;
+            }
         }
-        if (dest.x < source.x && dest.y < source.y) {
-            getStep(source, dest, steps, step, sign2, sign2);
-        }
-        if (dest.x > source.x && dest.y > source.y) {
-            getStep(source, dest, steps, step, sign1, sign1);
-        }
-        if (dest.x < source.x && dest.y > source.y) {
-            getStep(source, dest, steps, step, sign2, sign1);
-        }
+        steps[step] = dest;
         return steps;
     }
 
-    private void getStep(Cell source, Cell dest, Cell[] steps, int step, int dirX, int dirY) {
-        for (int i = 1; i <= abs(dest.x - source.x); i++) {
-            for (Cell cell : Cell.values()) {
-                if (cell.x == (source.x + i * dirX) && cell.y == (source.y + i * dirY)) {
-                    steps[step++] = cell;
-                }
-            }
-        }
-    }
 
     @Override
     public Figure copy(Cell dest) {
