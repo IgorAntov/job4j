8c32b6867ec6ba228c4239af500e6ec4adf319a2
IgorAntov <igor3377@yandex.ru>
2018-08-09 08:03:09 +0500

    1. Реализовать коллекцию Set на массиве[#65339]

---
+++ chapter_005/src/main/java/ru/job4j/set/SimpleSet.java
@@ -0,0 +1,44 @@
+package ru.job4j.set;
+
+import ru.job4j.generic.SimpleArray;
+import java.util.Iterator;
+
+/**
+ * @author Igor Antropov
+ * @version $Id$
+ * @since 0.1
+ */
+public class SimpleSet<T> implements Iterable<T> {
+
+    private SimpleArray<T> set;
+
+    public SimpleSet(int size) {
+        set = new SimpleArray<>(size);
+    }
+
+    /**
+     * Method adds new Object to List
+     */
+    public boolean add(T model) {
+        Iterator<T> it = set.iterator();
+        boolean isObj = false;
+        boolean result = false;
+        while (it.hasNext()) {
+            T temp = it.next();
+            if (model.equals(temp)) {
+                isObj = true;
+                break;
+            }
+        }
+        if (!isObj) {
+            set.add(model);
+            result = true;
+        }
+        return result;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return set.iterator();
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/set/SimpleSetTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.set;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * @author Igor Antropov
+ * @version $Id$
+ * @since 0.1
+ */
+public class SimpleSetTest {
+
+    public SimpleSet<String> simpleSet = new SimpleSet<>(5);
+    public Iterator it;
+
+    @Test
+    public void addObjectsStringInSet() {
+        simpleSet.add("test1");
+        simpleSet.add("test2");
+        simpleSet.add("test3");
+        simpleSet.add("test2");
+
+        it = simpleSet.iterator();
+        assertThat(it.next(), is("test1"));
+        assertThat(it.next(), is("test2"));
+        assertThat(it.next(), is("test3"));
+        assertThat(it.hasNext(), is(false));
+    }
+}
