0439abf5e495b97739ab8638a86b3af515f3c9e7
IgorAntov <igor3377@yandex.ru>
2018-08-18 07:19:32 +0500

    8. Реализовать собственную структуру данных - HashMap[#66420]

---
+++ chapter_005/src/main/java/ru/job4j/map/SimpleHashMap.java
@@ -0,0 +1,185 @@
+package ru.job4j.map;
+
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * @author Igor Antropov
+ * @version $Id$
+ * @since 0.1
+ */
+public class SimpleHashMap<K,V> implements Iterable<K> {
+
+    private int count;
+    private int modCount;
+    private int capacity_increment = 3;
+    private Node<K, V>[] table = new Node[capacity_increment];
+
+    /**Entry to Array
+     * @param <K> - key
+     * @param <V> - value
+     */
+    static class Node<K, V> {
+        final K key;
+        V value;
+
+        Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public final K getKey() {
+            return key;
+        }
+
+        public final V getValue() {
+            return value;
+        }
+    }
+
+    /**
+     * Method evaluates hash for key
+     * @param key
+     * @return
+     */
+    static final int hash(Object key) {
+        int h;
+        return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
+    }
+
+    /**
+     * Method inserts new Entry (key, value) to array
+     * @param key
+     * @param value
+     * @return
+     */
+    boolean insert(K key, V value) {
+        boolean result;
+        // int index = (table.length - 1) & hash(key);
+        int index = hash(key);
+        Node<K, V> p = table[index];
+        if (p == null) {
+            table[index] = new Node(key, value);
+            this.count++;
+            this.modCount++;
+            if (count == table.length - 1) {
+                table = resize(table.length);
+            }
+            result = true;
+        } else {
+            result = false;
+        }
+
+        return result;
+    }
+
+    /** Method gets entry - Node from Array
+     * @param key
+     * @return
+     */
+    public V get(K key) {
+        V result = null;
+        if (key != null) {
+            if (table[hash(key)] != null) {
+                result = table[hash(key)].getValue();
+            }
+        }
+        return result;
+    }
+
+    /** Method deletes entry from Array
+     * @param key
+     * @return
+     */
+    boolean delete(K key) {
+        boolean result = false;
+        if (key != null) {
+            if (table[hash(key)] != null) {
+                table[hash(key)] = null;
+                this.modCount++;
+                this.count--;
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Method resize capacity of table array
+     * @param - old array length
+     * @return
+     */
+    private Node<K, V>[] resize(int length) {
+        Node<K, V>[] newTab = (Node<K, V>[]) new Node[length + capacity_increment];
+        Node<K, V> e;
+        for (int j = 0; j < table.length; j++) {
+            e = table[j];
+            if (e != null) {
+//              if ((newTab[hash(e.key) & (newTab.length - 1)]) == null)  {
+//                  newTab[hash(e.key) & (newTab.length - 1)] = e;
+                if (newTab[hash(e.key)] == null) {
+                    newTab[hash(e.key)] = e;
+
+                } else {
+                    newTab = resize(newTab.length);
+                    break;
+                }
+            }
+        }
+        return newTab;
+    }
+
+    /**
+     *  Method returns entries amount in the array
+     */
+    public int size() {
+        return this.count;
+    }
+
+    /**
+     *Method implements iterable interface
+     * @return key iterator
+     */
+    @Override
+    public Iterator<K> iterator() {
+        return new Iterator<K>() {
+
+            int expectedModCount = modCount;
+            int indexIterator = 0;
+            int pointer = 0;
+
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                boolean result = false;
+                if (indexIterator < count) {
+                    result = true;
+                }
+                return result;
+            }
+
+            @Override
+            public K next() {
+                if (!this.hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                K key = null;
+                while (pointer < table.length) {
+                    if (table[pointer] != null) {
+                        key = table[pointer++].getKey();
+                        indexIterator++;
+                        break;
+                    } else {
+                        pointer++;
+                    }
+                }
+                return key;
+            }
+        };
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/map/SimpleHashMapTest.java
@@ -0,0 +1,58 @@
+package ru.job4j.map;
+
+import org.junit.Test;
+import java.util.Iterator;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Igor Antropov
+ * @version $Id$
+ * @since 0.1
+ */
+public class SimpleHashMapTest {
+
+    @Test
+    public void SimpleHashMapTest() {
+
+        class Key {
+            private int i;
+            public Key(int i) {
+                this.i = i;
+            }
+            @Override
+            public int hashCode() {
+                return i;
+            }
+        }
+
+        SimpleHashMap<Key, Integer> simpleHashMap = new SimpleHashMap<>();
+        Key first = new Key(1);
+        assertThat(simpleHashMap.insert(first, 1), is(true));
+        Key second = new Key(2);
+        assertThat(simpleHashMap.insert(second, 2), is(true));
+        simpleHashMap.insert(new Key(3), 3);
+        simpleHashMap.insert(new Key(4), 4);
+        simpleHashMap.insert(new Key(5), 5);
+        simpleHashMap.insert(new Key(6), 6);
+        simpleHashMap.insert(new Key(7), 7);
+        simpleHashMap.insert(new Key(8), 8);
+        simpleHashMap.insert(new Key(9), 9);
+        assertThat(simpleHashMap.delete(first), is(true));
+        assertThat(simpleHashMap.size(), is(8));
+        assertThat(simpleHashMap.get(second), is(2));
+
+        Iterator<Key> it = simpleHashMap.iterator();
+        assertThat(it.next().i, is(2));
+        assertThat(it.next().i, is(3));
+        assertThat(it.next().i, is(4));
+        assertThat(it.next().i, is(5));
+        assertThat(it.next().i, is(6));
+        assertThat(it.next().i, is(7));
+        assertThat(it.next().i, is(8));
+        assertThat(it.next().i, is(9));
+        assertThat(it.hasNext(), is(false));
+    }
+}
+
