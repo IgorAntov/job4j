38727031725e46b14af7c734275bfaf9a5f0ef4b
IgorAntov <igor3377@yandex.ru>
2018-09-04 06:55:53 +0500

    3. Реализовать механизм программной остановки потока.[#69669]

--- chapter_006/src/main/java/ru/job4j/pingpong/RectangleMove.java
+++ chapter_006/src/main/java/ru/job4j/pingpong/RectangleMove.java
@@ -28,6 +28,7 @@ public class RectangleMove implements Runnable {
                 Thread.sleep(50);
             } catch (InterruptedException e) {
                 e.printStackTrace();
+                break;
             }
         }
     }
