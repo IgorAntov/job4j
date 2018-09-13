99d4961da3467ad618f6a59a2fb904dd1c50779a
IgorAntov <igor3377@yandex.ru>
2018-09-13 16:52:37 +0500

    3. ThreadSafe динамический список[#69675]

--- chapter_006/src/main/java/ru/job4j/monitor/arrays/MtArrayList.java
+++ chapter_006/src/main/java/ru/job4j/monitor/arrays/MtArrayList.java
@@ -1,4 +1,4 @@
-package ru.job4j.arrays;
+package ru.job4j.monitor.arrays;
 
 import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
@@ -11,8 +11,9 @@ public class MtArrayList<E> {
 
     @GuardedBy("this")
     private final SimpleDynamicArray<E> array = new SimpleDynamicArray<>();
+    volatile boolean doCopy;
 
-    synchronized public boolean add( E value) {
+    synchronized public boolean add(E value) {
         boolean result = false;
         if (this.array.add(value)) {
             result = true;
@@ -20,11 +21,41 @@ public class MtArrayList<E> {
         return result;
     }
 
+    /**
+     * Method gets object from array
+     * @param index
+     * @return
+     */
     synchronized public E get(int index) {
         return this.array.get(index);
     }
 
-    synchronized public Iterator <E> iterator() {
-        return array.iterator();
+    /**
+     * Iterator in fail-save mode
+     * @return
+     */
+    synchronized public Iterator<E> iterator() {
+        return copy(this.array).iterator();
+    }
+
+    private SimpleDynamicArray<E> copy(SimpleDynamicArray<E> array) {
+        SimpleDynamicArray<E> tempArray = new SimpleDynamicArray<>();
+        synchronized (array) {
+            try {
+                if (doCopy) {
+                    array.wait();
+                }
+                doCopy = true;
+                Iterator<E> it = array.iterator();
+                while (it.hasNext()) {
+                    tempArray.add(it.next());
+                }
+                doCopy = false;
+                array.notifyAll();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return tempArray;
     }
 }
