0f787abbbf71c11a79e64cfdea21e4209979b774
IgorAntov <igor3377@yandex.ru>
2019-01-24 13:27:38 +0500

    Log4j2. Логирование системы[#108347]

--- chapter_007/src/main/java/ru/job4j/principle_004/UsageLog4j2.java
+++ chapter_007/src/main/java/ru/job4j/principle_004/UsageLog4j2.java
@@ -1,8 +1,7 @@
 package ru.job4j.principle_004;
 
-import org.apache.log4j.Logger;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.xml.DOMConfigurator;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 /**
  * @author Igor Antropov
@@ -14,7 +13,6 @@ public class UsageLog4j2 {
     private static final Logger LOG = LogManager.getLogger(UsageLog4j2.class.getName());
 
     public static void main(String[] args) {
-        DOMConfigurator.configure("chapter_007/src/main/resources/log4j2.xml");
         LOG.trace("trace message");
         LOG.debug("debug message");
         LOG.info("info message");
