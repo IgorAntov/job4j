7328af05b9a2e0f2c4d4907c8cee3909ca42bb00
IgorAntov <igor3377@yandex.ru>
2019-02-28 14:36:20 +0500

    Создать программу 'Консольный чат'.[#112590] - исправление ошибок

--- chapter_009/src/main/java/ru/job4j/chat/input/ConsoleInput.java
+++ chapter_009/src/main/java/ru/job4j/chat/input/ConsoleInput.java
@@ -15,30 +15,4 @@ public class ConsoleInput  implements Input {
     public String ask() {
         return scanner.nextLine();
     }
-
-  /*
-
-  public String ask(String question) {
-      System.out.print(question);
-      return scanner.next();
-  }
-
-    public int ask(String question, List<Integer> range) {
-
-        int key = Integer.valueOf(this.ask(question));
-        boolean exist = false;
-        for (int value: range) {
-            if (value == key) {
-                exist = true;
-                break;
-            }
-        }
-        if (!exist) {
-            throw new MenuOutException("Выход за пределы диапазона значений.");
-        }
-        return key;
-    }
-
-   */
-
 }
