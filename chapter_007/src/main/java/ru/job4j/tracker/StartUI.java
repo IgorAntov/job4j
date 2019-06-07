ac05035e347b1b6e3c625e91c21edb45ad3d8f3a
IgorAntov <igor3377@yandex.ru>
2019-06-07 12:30:09 +0500

    StartUI fixing

--- chapter_007/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_007/src/main/java/ru/job4j/tracker/StartUI.java
@@ -10,6 +10,6 @@ public class StartUI {
     public static void main(String[] args) {
         ITracker tracker = new TrackerSQL(ConnectionDB.create());
         Input input = new ValidateInput(new ConsoleInput());
-        new ru.job4j.tracker.start.StartUI(input, tracker).init();
+        new ru.job4j.tracker.start.StartUI(input, tracker, System.out::println).init();
     }
 }
