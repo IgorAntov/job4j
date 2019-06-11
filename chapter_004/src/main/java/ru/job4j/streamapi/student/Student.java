4b9c9b243431c5934cb6ecae642978bb5841f933
IgorAntov <igor3377@yandex.ru>
2019-06-11 05:55:44 +0500

    1. Фильтрация учеников.[#148499]

---
+++ chapter_004/src/main/java/ru/job4j/streamapi/student/Student.java
@@ -0,0 +1,18 @@
+package ru.job4j.streamapi.student;
+
+/**
+ * @author Igor Antropov
+ * @version $Id$
+ * @since 0.1
+ */
+public class Student {
+    private int score;
+
+    public Student(int score) {
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
