1a908032094f6657b5108be3e241dc5d3e6b36f5
IgorAntov <igor3377@yandex.ru>
2019-07-08 12:54:05 +0500

    1. Создать web проект[#157587]

---
+++ chapter_012/src/main/java/ru/job4j/servlets/EchoServlet.java
@@ -0,0 +1,22 @@
+package ru.job4j.servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author Igor Antropov
+ * @version $Id$
+ * @since 0.1
+ */
+public class EchoServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        PrintWriter writer = new PrintWriter(res.getOutputStream());
+        writer.append("hello world");
+        writer.flush();
+    }
+}
